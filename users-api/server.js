76eefbb8f27238e301a3e10e004b485e8249d273
cjonagam <cjonagam@jjonagam-osx.local>
2016-03-03 14:05:38 -0800

    cors

--- users-api/server.js
+++ users-api/server.js
@@ -27,6 +27,13 @@ app.use(bodyParser.json());
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // =================================================================
 // routes ==========================================================
 // =================================================================
