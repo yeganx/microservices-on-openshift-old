307b66e46f3022a18d25f07a2f3ff2327d48130f
cjonagam <cjonagam@jjonagam-osx.local>
2016-03-07 12:19:36 -0800

    env var format

--- php-ui/index.php
+++ php-ui/index.php
@@ -40,7 +40,7 @@
                         <hr>
                     </div>
                     <div class="panel-body">
-                        <input type=hidden id="hdnTarget" value="<?=$_ENV["NODEJS_APPLICATION_DOMAIN"]; ?>">
+                        <input type=hidden id="hdnTarget" value="<?= getenv('NODEJS_APPLICATION_DOMAIN'); ?>">
                         <div class="row">
                             <div class="col-lg-12">
                                 <form id="login-form" action="#" method="post" role="form" style="display: block;">
