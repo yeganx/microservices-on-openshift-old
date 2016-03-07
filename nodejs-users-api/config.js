7457fa4a6ad4836116549b0aefbb478fb4a439de
cjonagam <cjonagam@jjonagam-osx.local>
2016-03-07 09:15:17 -0800

    config

--- nodejs-users-api/config.js
+++ nodejs-users-api/config.js
@@ -1,4 +1,11 @@
 module.exports = {
+	'secret': 'ilovescotchyscotch',
+	'database': 'mongodb://noder:noderauth@proximus.modulusmongo.net:27017/so9pojyN'
+};
+
+/*
+module.exports = {
 	'secret': 'saregama',
 	'database': 'mongodb://'+process.env.DATABASE_USER+':'+process.env.DATABASE_PASSWORD+'@'+process.env.DATABASE_SERVICE_NAME+'_HOST:27017/'+process.env.DATABASE_NAME
 };
+*/
