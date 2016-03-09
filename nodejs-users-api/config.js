98f0040702e69aa627845bb8a92d9b20515479ae
VeerMuchandi <veer.muchandi@gmail.com>
2016-03-09 17:48:46 -0500

    changed a few more params to match template

--- nodejs-users-api/config.js
+++ nodejs-users-api/config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
 	'secret': 'saregama',
-	'database': 'mongodb://'+process.env.DATABASE_USER+':'+process.env.DATABASE_PASSWORD+'@'+process.env.MONGO_SERVICE_HOST+':'+process.env.MONGO_SERVICE_PORT+'/'+process.env.DATABASE_NAME
+	'database': 'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGO_SERVICE_HOST+':'+process.env.MONGO_SERVICE_PORT+'/'+process.env.MONGODB_DATABASE
 };
