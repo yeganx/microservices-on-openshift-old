81c56050c554dc49edd8f73a8a7422c2b7cdd4b2
VeerMuchandi <veer.muchandi@gmail.com>
2016-03-10 09:05:07 -0500

    fixed the env variables

--- nodejs-users-api/config.js
+++ nodejs-users-api/config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
 	'secret': 'saregama',
-	'database': 'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGO_SERVICE_HOST+':'+process.env.MONGO_SERVICE_PORT+'/'+process.env.MONGODB_DATABASE
+	'database': 'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_SERVICE_HOST+':'+process.env.MONGODB_SERVICE_PORT+'/'+process.env.MONGODB_DATABASE
 };
