9cef501b2ca3b727e2413a9c6bd270754364c3eb
VeerMuchandi <veer.muchandi@gmail.com>
2016-03-10 22:50:07 -0500

    added some debug code

--- nodejs-users-api/config.js
+++ nodejs-users-api/config.js
@@ -1,4 +1,9 @@
+var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+var mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
+var mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
+var mongoUser = process.env.MONGODB_USER
 
+console.log("mongohost :"+mongoHost);
 module.exports = {
 	'secret': 'saregama',
 	'database': 'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_SERVICE_HOST+':'+process.env.MONGODB_SERVICE_PORT+'/'+process.env.MONGODB_DATABASE
