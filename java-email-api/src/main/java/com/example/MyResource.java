82ab98d5b1796c8f0296139ce92610d60f7c52d4
System Administrator <root@jjonagam-osx.local>
2016-03-20 19:05:50 -0400

    added java microservice to fetch user tweets

--- java-email-api/src/main/java/com/example/MyResource.java
+++ java-email-api/src/main/java/com/example/MyResource.java
@@ -1,28 +1,70 @@
 package com.example;
+ 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import org.json.simple.JSONObject;
+import javax.ws.rs.core.Response;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("/")
 public class MyResource {
+    
+    @GET
+    @Path("getTweets")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getNewAccountNumber(@QueryParam("name") String name) {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET 
-    @Produces("application/json")
-    public JSONObject getIt() {
-        JSONObject myObject = new JSONObject();
-        myObject.put("name", "Agamemnon");
-        return myObject;
+        User u = new User();
+        List<String> tweetsFromUser = new ArrayList<String>();
+
+        try {
+
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey("DPvTS3jiK9FA5A51FwkIFO3l7")
+                    .setOAuthConsumerSecret(
+                            "ss44Xy3S9lmwAQSe3kbseVmyZmpjeopIXtH7fUTqtZqDW5FICs")
+                    .setOAuthAccessToken(
+                            "281649701-ZjJVyFrgeG1OtEOGuzmcwGCj7WzxZvlsRJVM70CV")
+                    .setOAuthAccessTokenSecret(
+                            "XyKNiKsABTc9bMvxmIrJ0w2hsR6k3Fdu1gHdF54W2CVYu");
+            TwitterFactory tf = new TwitterFactory(cb.build());
+            Twitter twitter = tf.getInstance();
+
+            Query query = new Query(name);
+            QueryResult result;
+            do {
+                result = twitter.search(query);
+                List<twitter4j.Status> tweets = result.getTweets();
+                for (twitter4j.Status tweet : tweets) {
+                    System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+                    tweetsFromUser.add(tweet.getText());
+                }
+            } while ((query = result.nextQuery()) != null);
+            
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            tweetsFromUser.add("Exception occurred..!!!");
+        }
+
+        u.setTweets(tweetsFromUser);
+
+        return Response.status(200).entity(u).build();
     }
+
 }
