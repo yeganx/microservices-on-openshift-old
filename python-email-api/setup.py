8126df8a19e12c7707159577af2ce6d349be7337
Dynamo Stack <9chakri+ds@gmail.com>
2016-03-21 12:34:50 -0400

    port change

--- python-email-api/setup.py
+++ python-email-api/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup (
-    name             = "sample:api",
+    name             = "sample:api -b 0.0.0.0:8080",
     version          = "0.1",
     description      = "Example application to be deployed.",
     packages         = find_packages(),
