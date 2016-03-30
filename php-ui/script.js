5bf934f72c43f99e88c6f6396450dea1793f68b0
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-03-30 08:38:09 -0700

    Update script.js

--- php-ui/script.js
+++ php-ui/script.js
@@ -20,7 +20,7 @@ app.controller('appController', function($scope,$http) {
 		$scope.token='';
 	});
     $scope.login = function (){
-	  	$http.post($scope.target+"/api/authenticate",$scope.loginform).success(function(data, status) {
+	  	$http.post("http://userregsvc-"+$scope.target+"/api/authenticate",$scope.loginform).success(function(data, status) {
             if(data['success']==true){
             	alert('Login successful, click link to get friends list.');
             	$scope.token=data.token;
@@ -31,21 +31,21 @@ app.controller('appController', function($scope,$http) {
         });
 	};
 	$scope.getFriendsList=function(){
-	    $http.get($scope.target+"/api/users?token="+$scope.token).success(function(data, status) {
+	    $http.get("http://userregsvc-"+$scope.target+"/api/users?token="+$scope.token).success(function(data, status) {
 	            console.log(data);
 	            $scope.friends=data;
 	            $scope.currentPage='friends';
             });	
 	};
 	$scope.getTweets=function(user){
-	   $http.get($scope.tweetsTarget+"/simple-service-webapp/tweets/tweets?name="+user).success(function(data, status) {
+	   $http.get("http://tweetssvc-"+$scope.target+"/simple-service-webapp/tweets/tweets?name="+user).success(function(data, status) {
 		    console.log(data);
 	            $scope.tweets=data;
 	            $scope.currentPage='tweets';
             });	
 	}
 	$scope.register = function (){
-	  	$http.post($scope.target+"/users", $scope.form).success(function(data, status) {
+	  	$http.post("http://userregsvc-"+$scope.target+"/users", $scope.form).success(function(data, status) {
             if(data['success']==true){
             	alert('Registration successful, please login');
             	$scope.currentPage='login';
